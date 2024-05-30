aec69374f7dce1fd26bd06cf435f82a98988b1c8
VictoriaVihreva <121255040+VictoriaVihreva@users.noreply.github.com>
2024-05-30 20:00:03 +0300

    Update main.cpp

--- samples/main.cpp
+++ samples/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    /*Polinom p1("-7y-15xyz+x^2y");
+    Polinom p1("-7y-15xyz+x^2y");
     Polinom p2("y-12+5xyz+45x^2y^4z^3");
     cout << "p1 = ";
     p1.Print();
@@ -21,12 +21,12 @@ int main()
     res.Print();
     res = p1 * 3;
     cout << "\np1 * 3 = ";
-    res.Print();*/
+    res.Print();
 
-    Polinom pol1("12x^2yz+12+11x");
+    /*Polinom pol1("12x^2yz+12+11x");
     Polinom pol2("6x+25x^2y^2z^2+5");
     Polinom pol3 = pol1 + pol2;
     Polinom pol4("25x^2y^2z^2+12x^2yz+17x+17");
-    pol3.Print();
+    pol3.Print();*/
 }
 
